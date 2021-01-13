c2ce829c0650cb865c54e552f82fb3d4c65d7a3a
oleh <olegstatkevychb@gmail.com>
2021-01-13 21:09:58 +0200

    add open/close sidebar

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,8 +1,8 @@
 <template>
 <div class="app-main-layout">
-  <Navbar/>
-  <Sidebar/>
-  <main class="app-content">
+  <Navbar @click="isOpen = !isOpen"/>
+  <Sidebar v-model="isOpen"/>
+  <main class="app-content" :class="{full: !isOpen}">
     <div class="app-page">
       <router-view />
     </div>
@@ -20,6 +20,9 @@ import Navbar from '@/components/apps/Navbar';
 import Sidebar from '@/components/apps/Sidebar';
 export default {
   name:'main-layout',
+  data:()=>({
+    isOpen:true
+  }),
   components:{
     Navbar,Sidebar
   }
