05b31c65cf6fda0a864d11f1619f31db5a39d4be
oleh <olegstatkevychb@gmail.com>
2021-01-17 12:46:40 +0200

    add +register message plugin

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import Vuelidate from "vuelidate";
 import App from "./App.vue";
 import "./registerServiceWorker";
 import dataFilter from "@/filters/data.filter";
+import messagePlugin from "@/utils/message.plugin";
 import router from "./router";
 import store from "./store";
 import 'materialize-css/dist/js/materialize.min'
@@ -10,6 +11,7 @@ import 'materialize-css/dist/js/materialize.min'
 Vue.config.productionTip = false;
 Vue.filter('data',dataFilter);
 Vue.use(Vuelidate);
+Vue.use(messagePlugin);
 new Vue({
   router,
   store,
